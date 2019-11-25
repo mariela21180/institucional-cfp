815807782f0158c7018e479c567f339836922850
Juan Salerno <salerno_juanig@hotmail.com>
2019-11-25 18:27:35 -0300

    #12 Comienzo DTO de clases del modulo Persona

---
+++ proyecto/src/persona/dto/alumno-dto.ts
@@ -0,0 +1,5 @@
+export class AlumnoDto {
+    private readonly datos: any; // ver que tipo de dato pongo acá (y si va algo)
+    private readonly nivelEstudioAlcanzado: string;
+    private readonly adeudaDocumentacion: boolean;
+}
