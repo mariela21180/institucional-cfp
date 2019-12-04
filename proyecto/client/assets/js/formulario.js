f1be61c485a579caf0d45d7b29fcf0701c9a38cb
OzielSpathe <ozielspathe@gmail.com>
2019-12-04 19:55:57 -0300

    js

---
+++ proyecto/client/assets/js/formulario.js
@@ -0,0 +1,20 @@
+/*Cuando tocas guardar llenar el formulario en la tabla
+y refrescar la tabla y cambiar la paguina al home de formulario*/
+
+let btnGuardarFormulario = document.getElementById('btnGuardarFormulario');
+let nombreFormulario = document.getElementById('nombreFormulario');
+let descripcion = document.getElementById('descripcion');
+let tblFormulario = document.getElementById('tblFormulario');
+let mock = [
+    {'nombreformulario':'Cruso',
+'descripcion':'curso programador full stack'
+},
+{'nombreformulario':'Alumnos',
+'descripcion':'Inscrpción de Alumnos'
+},
+{'nombreformulario':'Asistencias',
+'descripcion':'Cómputo de asistencias'
+}
+];
+
+let campos = [];
