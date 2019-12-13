6e511a0fff6404f6bb6a8d611887b22999b934ae
agusnotti <agustinantt@gmail.com>
2019-12-13 18:32:57 -0300

    #8 - Agregado login y avances en js

--- proyecto/client/assets/js/itemNavActivo.js
+++ proyecto/client/assets/js/itemNavActivo.js
@@ -1,8 +1,13 @@
 function activarNav(){
     let itemLinks = document.querySelectorAll('.nav-link');
     let navItems = document.querySelectorAll('.nav-item.active');
+    let cerrarSesionBtn = document.getElementById('cerrar-sesion');
 
     activarItemClick();
+
+    cerrarSesionBtn.addEventListener('click', function () { 
+        window.location.href = window.location.origin;
+    });
     
     function activarItemClick() {
         let url = window.location.pathname;
