1b260395650ba994a6edbfd335cdc4a503affd11
Mariela Gonzalez <mariela21180@gmail.com>
2019-12-14 17:11:06 -0300

    #14 - Modificando Telefono para que sea uno solo y requerido, y modificando persona para que levante la info del telefono tambien

--- - BD/scripts_bd.sql
+++ - BD/scripts_bd.sql
@@ -1,16 +1,19 @@
+
+create database if not exists institucional_cfl;
 use `institucional_cfl`;
 
-insert into persona values (1, "Mariela", "Gonzalez", 28200912, "mariela@gmail.com");
-insert into persona values (2, "Juan", "Salerno", 2000000, "juan@gmail.com");
-insert into persona values (3, "Nahum", "Larzabal", 2000001, "nahum@gmail.com");
 
 insert into domicilio(idDomicilio, calle, altura, idPersona) values (1, "Alberdi", 763, 2);
 insert into domicilio(idDomicilio, calle, altura, idPersona) values (2, "25 de Mayo", 963, 1);
 insert into domicilio(idDomicilio, calle, altura, idPersona) values (3, "Canada", 123, 3);
 
 insert into telefono values (1, 0249, 154633817, 1);
-insert into telefono values (2, 0249, 4420983, 1);
-insert into telefono values (3, 0249, 154997785, 2);
+insert into telefono values (2, 0249, 4420983, 2);
+insert into telefono values (3, 0249, 154997785, 3);
+
+insert into persona values (1, "Mariela", "Gonzalez", 28200912, "mariela@gmail.com", 1, 1);
+insert into persona values (2, "Juan", "Salerno", 2000000, "juan@gmail.com", 2, 2);
+insert into persona values (3, "Nahum", "Larzabal", 2000001, "nahum@gmail.com", 3, 3);
 
 insert into docente values (1, "Universitario Incompleto", "Lisenciado en Administración");
 insert into docente values (2, "Universitario Completo", "Contador Publico");
@@ -23,7 +26,7 @@ insert into usuario values (1, "mariela21180", "testpass123", 1);
 
 -- insert into asistencia values (1, 1);
 
-insert into curso values (1, "Programador Full-Stack", "-", 30, "2019-01-02", "2019-12-02", 1, 0.8, 180.5);
+insert into curso values (1, "Programador Full-Stack", "-", 30, 0.8, 180.5, "2019-01-02", "2019-12-02", 1);
 
 insert into clase values (1, 1, "2019-01-02 19:00:00", "2019-01-02 22:30:00");
 
@@ -37,13 +40,13 @@ insert into horario values (1, "Lunes", "19hs", "22:30hs", 1);
 
 insert into formulario values (1, 1, "Examen 1", "MySQL");
 
-insert into examen values (1, 1, 1, 1, 5.8);
+insert into examen values (1, 5.8, 1, 1, 1);
 
 insert into tipo_pregunta values (1, "Texto");
 
-insert into pregunta values (1, 1, "¿Como me llamo?", 1, 1, 1, 2.5);
+insert into pregunta values (1, 1, "¿Como me llamo?", 1, 2.5, 1, 1);
 
-insert into opcion values (1, "Mariela", 1, 1, 2.5);
+insert into opcion values (1, "Mariela", 1, 2.5, 1);
 
 
 
