8df52a661b1cb3f7f8d5f52aca1a1651d6b2ecd3
Juan Salerno <salerno_juanig@hotmail.com>
2019-12-14 17:43:52 -0300

    #14 - Controller y Service de Material

--- proyecto/src/curso/controllers/material.controller.ts
+++ proyecto/src/curso/controllers/material.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Param, Put } from '@nestjs/common';
+import { MaterialService } from '../services/material.service';
 
-@Controller('material')
-export class MaterialController {}
+@Controller('materiales')
+export class MaterialController {
+    public constructor(private materialService: MaterialService) {}
+    
+    @Get()
+    async getMaterials() {
+        return await this.materialService.getMaterials();
+    }
+
+    @Get(':materialId')
+    async getMaterial(@Param('materialId') materialId: any) {
+        return await this.materialService.getMaterial(parseInt(materialId));
+    }
+
+    // @UseGuards(AuthGuard())
+    @Post()
+    async addMaterial(@Body() material: any) {
+        return await this.materialService.addMaterial(material);
+    }
+    
+    // @UseGuards(AuthGuard())
+    @Delete(':materialId')
+    async deleteMaterial(@Param('materialId') materialId: any) {
+        return await this.materialService.deleteMaterial(parseInt(materialId));
+    }
+    
+    // @UseGuards(AuthGuard())
+    @Put(':materialId')
+    async updateMaterial(@Param('materialId') materialId: any, @Body() material: any) {
+        return await this.materialService.updateMaterial(parseInt(materialId), material);
+    }
+}
--- proyecto/src/curso/dto/material-dto.ts
+++ proyecto/src/curso/dto/material-dto.ts
@@ -1,4 +1,4 @@
 export class MaterialDto {
-    readonly habilitado?: boolean;
     readonly idClase: number;
+    readonly habilitado?: boolean;
 }
