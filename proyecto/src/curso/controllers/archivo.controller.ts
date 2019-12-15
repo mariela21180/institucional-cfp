8e148e2a3c4c457bc2956a633b7624fe5801b7e7
Juan Salerno <salerno_juanig@hotmail.com>
2019-12-15 14:43:43 -0300

    #14 - Controller y service Archivo, con correccion en entity

--- proyecto/src/curso/controllers/archivo.controller.ts
+++ proyecto/src/curso/controllers/archivo.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put } from '@nestjs/common';
+import { ArchivoService } from '../services/archivo.service';
 
-@Controller('archivo')
-export class ArchivoController {}
+@Controller('archivos')
+export class ArchivoController {
+    public constructor(private archivoService: ArchivoService) {}
+    
+    @Get()
+    async getArchivos() {
+        return await this.archivoService.getArchivos();
+    }
+
+    @Get(':archivoId')
+    async getArchivo(@Param('archivoId') archivoId: any) {
+        return await this.archivoService.getArchivo(parseInt(archivoId));
+    }
+
+    // @UseGuards(AuthGuard())
+    @Post()
+    async addArchivo(@Body() archivo: any) {
+        return await this.archivoService.addArchivo(archivo);
+    }
+    
+    // @UseGuards(AuthGuard())
+    @Delete(':archivoId')
+    async deleteArchivo(@Param('archivoId') archivoId: any) {
+        return await this.archivoService.deleteArchivo(parseInt(archivoId));
+    }
+    
+    // @UseGuards(AuthGuard())
+    @Put(':archivoId')
+    async updateArchivo(@Param('archivoId') archivoId: any, @Body() archivo: any) {
+        return await this.archivoService.updateArchivo(parseInt(archivoId), archivo);
+    }
+}
