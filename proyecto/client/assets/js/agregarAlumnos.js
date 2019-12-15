a5749cab9d944136d4d0b194b2209703c9ed9a0a
agusnotti <agustinantt@gmail.com>
2019-12-15 03:02:18 -0300

    #16 funcionalidad agregarAlumnos (post)

--- proyecto/client/assets/js/agregarAlumnos.js
+++ proyecto/client/assets/js/agregarAlumnos.js
@@ -1,4 +1,4 @@
-import { async } from "rxjs/internal/scheduler/async";
+///import { async } from "rxjs/internal/scheduler/async";
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
@@ -9,62 +9,100 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let documentoAlumno = document.getElementById('documentoAlumno');
     let eMailAlumno = document.getElementById('eMailAlumno');
     let calleAlumno = document.getElementById('calleAlumno');
-    let alturaAlumno = document.getElementById('alturaAlumno');
+    let alturaDomicilioAlumno = document.getElementById('alturaDomicilioAlumno');
     let pisoAlumno = document.getElementById('pisoAlumno');
     let dptoAlumno = document.getElementById('dptoAlumno');
     let nivelAlumno = document.getElementById('nivelAlumno'); // tiene un select tde value 1,2,3,4,5
-
+    let codAreaAlumno = document.getElementById('codAreaAlumno');
+    let telefonoAlumno = document.getElementById('telefonoAlumno');
 
     guardarAlumno.addEventListener('click', crearAlumno); //llama a la funcion (solo en el addEventListener)
 
     function crearAlumno() { 
-        let curso = armarAlumno();
-        console.log(curso);
-        
-        //llamadaAjax post curso
+        let persona = armarPersona();        
+        agregarPersonaServidor(persona).then((p) => {
+            
+            if(p.idPersona != null){
+                let alumno = armarAlumno(p.idPersona);
+                agregarAlumnoServidor(alumno);
+                
+                let domicilio = armarDomicilio(p.idPersona);
+                agregarDomicilioServidor(domicilio);
+
+                let telefono = armarTelefono(p.idPersona);
+                agregarTelefonoServidor(telefono);
+            }
+            
+        });
     }
 
-    async function agregarPersonaServidor(persona){
-       let p = await fetch ("/personas",{"method":"POST","headers":{"Content-Type":"application/json"},"body":JSON.stringify(persona)})
-       console.log(p) 
-       if(p.status != 404){
-            let json = await p.json();
-            persona.id = json.id            
+    async function agregarPersonaServidor(personadto){
+       let result = await fetch ("/personas",{"method":"POST","headers":{"Content-Type":"application/json"},"body":JSON.stringify(personadto)})
+       if(result.status != 404){
+            let json = await result.json();
+            personadto.idPersona = json.idPersona;                  
         }
+        return personadto;
     }
 
+    async function agregarAlumnoServidor(alumnodto){
+        let result = await fetch ("/alumnos",{"method":"POST","headers":{"Content-Type":"application/json"},"body":JSON.stringify(alumnodto)})
+        
+        if(result.status != 404){
+            let json = await result.json();          
+        }
+        return alumnodto;
+    }
 
+    async function agregarDomicilioServidor(domiciliodto){
+        let result = await fetch ("/domicilios",{"method":"POST","headers":{"Content-Type":"application/json"},"body":JSON.stringify(domiciliodto)});
+        if(result.status != 404){
+            let json = await result.json();          
+        }
+        return domiciliodto;
+    }
+
+    async function agregarTelefonoServidor(telefonodto){
+        let result = await fetch ("/telefonos",{"method":"POST","headers":{"Content-Type":"application/json"},"body":JSON.stringify(telefonodto)})
+        if(result.status != 404){
+            let json = await result.json();          
+        }
+        return telefonodto;
+    }
 
     function armarPersona() { 
-        let persona = {
+        return {
             'nombre': nombreAlumno.value,
             'apellido': apellidoAlumno.value,
             'dni': documentoAlumno.value,
             'eMail': eMailAlumno.value
         }
-        return persona;
+    }
+    
+    function armarAlumno(idPersona) { 
+        return {
+            'nivelEstudioAlcanzado': nivelAlumno.value, //
+            'adeudaDocumentacion': false,
+            'idPersona': idPersona
+        }
     }
 
-   /*  function armarDomicilio() { 
+   function armarDomicilio(idPersona) { 
         return {
             'calle': calleAlumno.value,
-            'altura': alturaAlumno.value,
+            'altura': alturaDomicilioAlumno.value,
             'piso?': pisoAlumno.value,
             'dpto?': dptoAlumno.value,
-            'idPersona': cuando creo una persona me devuelve un id [] investigar como buscarlo
+            'idPersona': idPersona
         }
     }
-    function armarAlumno() { 
+
+    function armarTelefono(idPersona) { 
         return {
-            'nivelEstudioAlcanzado': nombreAlumno.value,
-            'adeudaDocumentacion': false
-            'idPersona': cuando creo una persona me devuelve un id [] investigar como buscarlo
+            'codArea': codAreaAlumno.value,
+            'nro': telefonoAlumno.value,
+            'idPersona': idPersona
         }
     }
-    function armarTelefono() { 
-        return {
-            objeto dentro de objeto
-        }
-    }
- */
+ 
 });
