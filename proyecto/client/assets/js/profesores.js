741e8f8212b9b295945934072295f32ec68d72e6
ozielspathe <ozielspathe@gmail.com>
2019-12-16 11:35:40 -0300

    modificacion de nombre en las variables

--- proyecto/client/assets/js/profesores.js
+++ proyecto/client/assets/js/profesores.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    let guardarAlumno = document.getElementById('guardarAlumno');
+    let guardarProfesor = document.getElementById('guardarProfesor');
 
     let nombreProfesor = document.getElementById('nombreProfesor');
     let apellidoProfesor = document.getElementById('apellidoProfesor');
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let telefonoProfesor = document.getElementById('telefonoProfesor');
     let tituloProfesor= document.getElementById('tituloProfesor');
 
-    guardarAlumno.addEventListener('click', crearProfesor); //llama a la funcion (solo en el addEventListener)
+    guardarProfesor.addEventListener('click', crearProfesor); //llama a la funcion (solo en el addEventListener)
 
     function crearProfesor() { 
         let persona = armarPersona();        
@@ -71,16 +71,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     function armarPersona() { 
         return {
-            'nombre': nombreAlumno.value,
-            'apellido': apellidoAlumno.value,
-            'dni': documentoAlumno.value,
-            'eMail': eMailAlumno.value
+            'nombre': nombreProfesor.value,
+            'apellido': apellidoProfesor.value,
+            'dni': dniProfesor.value,
+            'eMail': emailProfesor.value
         }
     }
     
     function armarProfesor(idPersona) { 
         return {
-            'nivelEstudioAlcanzado': nivelAlumno.value, //
+            'nivelEstudioAlcanzado': nivelEstudiosProfesor.value, //
             'titulo': tituloProfesor.value,
             'idPersona': idPersona
         }
@@ -88,18 +88,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
    function armarDomicilio(idPersona) { 
         return {
-            'calle': calleAlumno.value,
-            'altura': alturaDomicilioAlumno.value,
-            'piso?': pisoAlumno.value,
-            'dpto?': dptoAlumno.value,
+            'calle': calleProfesor.value,
+            'altura': alturaDomicilioProfesor.value,
+            'piso?': pisoProfesor.value,
+            'dpto?': dptoProfesor.value,
             'idPersona': idPersona
         }
     }
 
     function armarTelefono(idPersona) { 
         return {
-            'codArea': codAreaAlumno.value,
-            'nro': telefonoAlumno.value,
+            'codArea': codAreaProfesor.value,
+            'nro': telefonoProfesor.value,
             'idPersona': idPersona
         }
     }
